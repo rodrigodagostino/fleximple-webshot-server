6fc1e287287f494ca95e58ebaf77e53b1a33d5dc
Rodrigo D’Agostino <rodrigo.dagostino@gmail.com>
2021-12-25 22:01:21 -0300

    Add watch and ignore_watch configurations

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,8 @@ module.exports = {
   apps: [{
     name: 'webshot',
     script: './index.js',
+    watch: true,
+    ignore_watch: [ 'node_modules', 'public' ],
     max_memory_restart: '200M',
     autorestart: true,
   }],
